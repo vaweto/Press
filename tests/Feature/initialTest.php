a596f88772018f3a0a0a3cc7c9527cf347034102
= <=>
2019-09-06 17:59:00 +0300

    set up testing

---
+++ tests/Feature/initialTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace vaweto\Press\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+
+class initialTest extends TestCase
+{
+    /** @test */
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
